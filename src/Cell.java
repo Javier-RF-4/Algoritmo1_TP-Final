5cd198192453490731e90b6b2758b93354ba8e78
javir <jroldan@estudiantes.unsam.edu.ar>
2025-05-18 18:26:45 -0300

    Primer Commit

---
+++ src/Cell.java
@@ -0,0 +1,15 @@
+public class Cell {
+    private Object content;
+
+    // Metodo constructor
+    public Cell(Object content){
+        this.content = content;
+    }
+
+    //Metodo Getter
+    public Object getContent(){
+        return content;
+    }
+
+
+}
